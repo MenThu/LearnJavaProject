7cf5eff630eb8bd494ee9689c93e802b756de41b
menthuguan <menthuguan@douyu.tv>
2022-10-19 12:29:14 +0800

    创建以及提交springboot_02_base_configuration

---
+++ LearnSprintBoot/springboot_02_base_configuration/src/main/java/com/example/Springboot02BaseConfigurationApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02BaseConfigurationApplication {
+
+    //https://www.bilibili.com/video/BV15b4y1a7yG/?p=3&spm_id_from=pageDriver&vd_source=b179710628b1030cd4fc4bb40461a728
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02BaseConfigurationApplication.class, args);
+    }
+
+}
